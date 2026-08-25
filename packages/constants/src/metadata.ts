1491b3ce897072554a8545abdd202a3a917b00e8
arabbohara <annies.sswift12@gmail.com>
2026-08-25 09:22:12 +0900

    fix: update site URLs to reflect new server address

--- packages/constants/src/metadata.ts
+++ packages/constants/src/metadata.ts
@@ -9,7 +9,7 @@ export const SITE_TITLE = "System Management";
 export const SITE_DESCRIPTION = "Manage work items, cycles, and product roadmaps in one place.";
 export const SITE_KEYWORDS =
   "project management, work items tracking, agile, scrum, kanban, collaboration, roadmaps, cycles";
-export const SITE_URL = "http://52.193.85.123/";
+export const SITE_URL = "http://13.230.86.17/";
 export const TWITTER_USER_NAME = "System Management";
 
 // Sites Metadata
@@ -18,5 +18,5 @@ export const SPACE_SITE_TITLE = "System Management Publish | Share boards and ro
 export const SPACE_SITE_DESCRIPTION = "Publish and share System Management boards and roadmaps.";
 export const SPACE_SITE_KEYWORDS =
   "project management, work items tracking, agile, scrum, kanban, collaboration, roadmaps, cycles";
-export const SPACE_SITE_URL = "http://52.193.85.123/";
+export const SPACE_SITE_URL = "http://13.230.86.17/";
 export const SPACE_TWITTER_USER_NAME = "";
